1385d94d3629e61804aff57753b1e42293412d82
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-09-01 15:45:24 +0600

    Navbar done.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./ui/button";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,18 +8,26 @@ import {
 
 function Nav() {
   const items = ["Home", "Skills", "Projects", "Blog", "Contact Me"];
+
   return (
-    <div className="z-50 w-full border-black text-white font-extrabold text-3xl">
-      <NavigationMenu>
-        <NavigationMenuList className=" backdrop-blur-md rounded-xl px-4 py-2 hover:cursor-pointer">
-          {items.map((item) => (
-            <NavigationMenuItem>
-              <NavigationMenuLink>{item}</NavigationMenuLink>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className="z-50 w-full">
+      <div className="flex items-center  justify-between backdrop-blur-md rounded-xl px-4 py-2">
+        {/* Left side: nav items */}
+        <NavigationMenu>
+          <NavigationMenuList className="flex items-center gap-6 text-white font-black">
+            {items.map((item, index) => (
+              <NavigationMenuItem key={index}>
+                <NavigationMenuLink>{item}</NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* Right side: Resume button */}
+        <Button>Resume</Button>
+      </div>
     </div>
   );
 }
+
 export default Nav;
